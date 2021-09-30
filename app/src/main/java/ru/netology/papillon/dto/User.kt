fbb36266aac05cd28a143ed41f88f5db79bd95d2
nicys <nicys2476@yandex.ru>
2021-09-30 13:47:18 +0700

    corrected User.kt

--- app/src/main/java/ru/netology/papillon/dto/User.kt
+++ app/src/main/java/ru/netology/papillon/dto/User.kt
@@ -1,9 +1,9 @@
 package ru.netology.papillon.dto
 
 data class User(
+    val avatar: String? = null,
     val idUser: Long = 0L,
     val login: String = "",
     val name: String = "",
-    val avatar: String? = null,
     val isMe: Boolean = false,
 )
