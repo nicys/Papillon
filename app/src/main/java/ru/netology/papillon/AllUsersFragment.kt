eae63b7665a06448f974a010ca9855f665685e2c
nicys <nicys2476@yandex.ru>
2021-09-30 14:34:39 +0700

    added AllUsersFragment.kt

---
+++ app/src/main/java/ru/netology/papillon/AllUsersFragment.kt
@@ -0,0 +1,46 @@
+package ru.netology.papillon
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.DividerItemDecoration
+import ru.netology.papillon.adapter.OnUserInteractionListener
+import ru.netology.papillon.adapter.UsersAdapter
+import ru.netology.papillon.databinding.FragmentAllUsersBinding
+import ru.netology.papillon.dto.User
+import ru.netology.papillon.viewmodel.UserViewModel
+
+class AllUsersFragment : Fragment() {
+
+    val viewModel: UserViewModel by viewModels(ownerProducer = ::requireParentFragment)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentAllUsersBinding.inflate(inflater, container, false)
+
+        val adapter = UsersAdapter(object : OnUserInteractionListener {
+            override fun onUserClicked(user: User) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+        binding.listOfUsers.adapter = adapter
+
+        binding.listOfUsers.addItemDecoration(
+            DividerItemDecoration(requireContext(),
+                DividerItemDecoration.VERTICAL
+            )
+        )
+
+
+        return binding.root
+    }
+
+}
