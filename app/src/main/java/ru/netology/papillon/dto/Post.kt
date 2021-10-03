b7e73c6573ad86371c88c255e2bb48898bf1a235
nicys <nicys2476@yandex.ru>
2021-10-03 14:10:42 +0700

    added data class Post

---
+++ app/src/main/java/ru/netology/papillon/dto/Post.kt
@@ -0,0 +1,21 @@
+package ru.netology.papillon.dto
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Post(
+    val id: Long = 0L,
+    val authorId: Long = 0L,
+    val author: String = "",
+    val authorAvatar: String? = null,
+    val published: String = "",
+    val content: String = "",
+    val likedByMe: Boolean = false,
+    val likesCount: Int = 0,
+    val shares: String = "",
+    val sharesCnt: Int = 0,
+    val videoAttach: String? = null,
+    val audioAttach: String? = null,
+    val imageAttach: String? = null,
+) : Parcelable
