533985cd0aace8d4b639b89c6377e557bdcaa2bd
nicys <nicys2476@yandex.ru>
2021-10-06 16:00:49 +0700

    added exten fun's

--- app/src/main/java/ru/netology/papillon/extensions/ExtenFun.kt
+++ app/src/main/java/ru/netology/papillon/extensions/ExtenFun.kt
@@ -1,5 +1,9 @@
 package ru.netology.papillon.extensions
 
+import ru.netology.papillon.dto.Job
+import ru.netology.papillon.dto.Post
+import ru.netology.papillon.dto.User
+import ru.netology.papillon.entity.*
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -7,7 +11,20 @@ fun Date.toString(format: String, locale: Locale = Locale.getDefault()): String 
     val formatter = SimpleDateFormat(format, locale)
     return formatter.format(this)
 }
-
 fun getCurrentDateTime(): Date {
     return Calendar.getInstance().time
 }
+
+fun User.toEntityUser() = UserEntity(idUser, avatar, login, name, surname, isMe)
+fun List<UserEntity>.toDtoUser(): List<User> = map(UserEntity::toDtoUser)
+fun List<User>.toEntityUser(): List<UserEntity> = map(User::toEntityUser)
+
+fun Job.toEntityJob() = JobEntity(id, company, position, start, finish, link)
+fun List<JobEntity>.toDtoJob() = map(JobEntity::toDtoJob)
+fun List<Job>.toJobEntity() = map(Job::toEntityJob)
+
+fun Post.toEntityPost() = PostEntity(id, authorId, author, authorAvatar,
+    published, content, likedByMe, likesCount, shares, sharesCnt, views,
+    viewsCnt, videoAttach, audioAttach, imageAttach)
+fun List<PostEntity>.toDtoPost() = map(PostEntity::toDtoPost)
+fun List<Post>.toEntityPost() = map(Post::toEntityPost)
