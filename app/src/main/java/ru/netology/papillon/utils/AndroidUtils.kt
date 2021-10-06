636f292493f22c3f92b660ffda96fc885dfd4130
nicys <nicys2476@yandex.ru>
2021-10-06 13:02:34 +0700

    corrected AndroidUtils.kt

--- app/src/main/java/ru/netology/papillon/utils/AndroidUtils.kt
+++ app/src/main/java/ru/netology/papillon/utils/AndroidUtils.kt
@@ -54,4 +54,13 @@ object AndroidUtils {
             time
         }
     }
+
+    fun Date.toString(format: String, locale: Locale = Locale.getDefault()): String {
+        val formatter = SimpleDateFormat(format, locale)
+        return formatter.format(this)
+    }
+
+    fun getCurrentDateTime(): Date {
+        return Calendar.getInstance().time
+    }
 }
