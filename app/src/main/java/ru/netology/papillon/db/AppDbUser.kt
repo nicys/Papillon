922c23ff6c6446e2ea53d02fe0fbf0277447d984
nicys <nicys2476@yandex.ru>
2021-10-06 16:17:17 +0700

    initial AppDbUser.kt

---
+++ app/src/main/java/ru/netology/papillon/db/AppDbUser.kt
@@ -0,0 +1,32 @@
+package ru.netology.papillon.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import ru.netology.papillon.dao.UserDao
+import ru.netology.papillon.entity.UserEntity
+
+@Database(
+    entities = [UserEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDbUser : RoomDatabase() {
+    abstract fun usersDao(): UserDao
+
+    companion object {
+        @Volatile
+        private var instance: AppDbUser? = null
+
+        fun getInstance(context: Context): AppDbUser {
+            return instance ?: synchronized(this) {
+                instance ?: buildDatabase(context).also { instance = it }
+            }
+        }
+
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(context, AppDbUser::class.java, "appuserb.db")
+                .build()
+    }
+}
