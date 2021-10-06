b7ffa094fffa61fb026060dd190896d48cbc2cf1
nicys <nicys2476@yandex.ru>
2021-10-06 14:16:20 +0700

    added param's to JobsFragment.kt

--- app/src/main/java/ru/netology/papillon/JobsFragment.kt
+++ app/src/main/java/ru/netology/papillon/JobsFragment.kt
@@ -5,55 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import ru.netology.papillon.adapter.JobsAdapter
+import ru.netology.papillon.adapter.OnJobInteractionListener
+import ru.netology.papillon.databinding.FragmentJobsBinding
+import ru.netology.papillon.dto.Job
+import ru.netology.papillon.viewmodel.JobViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [JobsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class JobsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    val viewModel: JobViewModel by viewModels(ownerProducer = ::requireParentFragment)
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_jobs, container, false)
-    }
+        val binding = FragmentJobsBinding.inflate(inflater, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment JobsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            JobsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val adapter = JobsAdapter(object : OnJobInteractionListener {
+            override fun onDeleteJob(job: Job) {}
+            override fun onEdinJob(job: Job) {}
+        })
+
+        binding.rvListOfJobs.adapter = adapter
+
+
+        return binding.root
     }
 }
