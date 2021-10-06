578ad16d80692929fb5706671472c287fba42f0f
nicys <nicys2476@yandex.ru>
2021-10-06 16:17:08 +0700

    initial AppDbPost.kt

---
+++ app/src/main/java/ru/netology/papillon/db/AppDbPost.kt
@@ -0,0 +1,32 @@
+package ru.netology.papillon.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import ru.netology.papillon.dao.PostDao
+import ru.netology.papillon.entity.PostEntity
+
+@Database(
+    entities = [PostEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDbPost : RoomDatabase() {
+    abstract fun postsDao(): PostDao
+
+    companion object {
+        @Volatile
+        private var instance: AppDbPost? = null
+
+        fun getInstance(context: Context): AppDbPost {
+            return instance ?: synchronized(this) {
+                instance ?: buildDatabase(context).also { instance = it }
+            }
+        }
+
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(context, AppDbPost::class.java, "apppost.db")
+                .build()
+    }
+}
