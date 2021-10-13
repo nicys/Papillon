edeac3e780e78aac33e058cf2e11e467676c3ac1
nicys <nicys2476@yandex.ru>
2021-10-13 13:10:57 +0700

    added PostViewModel.kt

--- app/src/main/java/ru/netology/papillon/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/papillon/viewmodel/PostViewModel.kt
@@ -1,4 +1,35 @@
 package ru.netology.papillon.viewmodel
 
-class PostViewModel {
-}
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import ru.netology.papillon.db.AppDbPost
+import ru.netology.papillon.dto.Post
+import ru.netology.papillon.repository.PostRepository
+import ru.netology.papillon.repository.PostRepositoryImpl
+
+private val empty = Post()
+
+class PostViewModel(application: Application): AndroidViewModel(application) {
+
+    private val repository: PostRepository = PostRepositoryImpl(
+        AppDbPost.getInstance(context = application).postsDao()
+    )
+    val data = repository.getAll()
+    val edited = MutableLiveData(empty)
+
+    fun savePost() {
+        edited.value?.let {
+            repository.savePost(it)
+        }
+        edited.value = empty
+    }
+
+    fun editPost(post: Post) {
+        edited.value = post
+    }
+
+    fun likedById(id: Long) = repository.likedById(id)
+    fun sharedById(id: Long) = repository.sharedById(id)
+    fun removedById(id: Long) = repository.removedById(id)
+ }
