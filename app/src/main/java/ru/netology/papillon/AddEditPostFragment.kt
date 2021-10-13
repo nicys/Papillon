cf3acd8afec5f1ba965724e6e035a2f1565f4fcc
nicys <nicys2476@yandex.ru>
2021-10-13 14:23:28 +0700

    added AddEditPostFragment.kt

--- app/src/main/java/ru/netology/papillon/AddEditPostFragment.kt
+++ app/src/main/java/ru/netology/papillon/AddEditPostFragment.kt
@@ -5,55 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import ru.netology.papillon.databinding.FragmentAddEditPostBinding
+import ru.netology.papillon.utils.AndroidUtils.hideKeyboard
+import ru.netology.papillon.utils.AndroidUtils.showKeyboard
+import ru.netology.papillon.utils.StringArg
+import ru.netology.papillon.viewmodel.PostViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AddEditPostFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AddEditPostFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    companion object {
+        var Bundle.textDataContent: String? by StringArg
+        var Bundle.textDataVideo: String? by StringArg
     }
+
+    private val viewModel: PostViewModel by viewModels(
+        ownerProducer = ::requireParentFragment)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_edit_post, container, false)
-    }
+        val binding = FragmentAddEditPostBinding.inflate(inflater, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddEditPostFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddEditPostFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        binding.etEditContent.requestFocus()
+        showKeyboard(binding.root)
+
+        arguments?.textDataContent?.let { binding.etEditContent.setText(it) }
+        arguments?.textDataVideo?.let { binding.etVideo.setText(it) }
+
+        binding.ok.setOnClickListener {
+            viewModel.changeContent(binding.etEditContent.text.toString())
+            viewModel.changeVideoURL(binding.etVideo.text.toString())
+            viewModel.savePost()
+            hideKeyboard(requireView())
+            findNavController().navigateUp()
+        }
+
+        return binding.root
     }
 }
