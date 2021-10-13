12445a1a850c4dc6dae360008e81b4c6fa807095
nicys <nicys2476@yandex.ru>
2021-10-13 14:50:06 +0700

    corrected PostsFragment.kt

--- app/src/main/java/ru/netology/papillon/PostsFragment.kt
+++ app/src/main/java/ru/netology/papillon/PostsFragment.kt
@@ -1,11 +1,16 @@
 package ru.netology.papillon
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import ru.netology.papillon.AddEditPostFragment.Companion.textDataContent
+import ru.netology.papillon.AddEditPostFragment.Companion.textDataVideo
 import ru.netology.papillon.adapter.OnPostInteractionListener
 import ru.netology.papillon.adapter.PostsAdapter
 import ru.netology.papillon.databinding.FragmentPostsBinding
@@ -24,15 +29,40 @@ class PostsFragment : Fragment() {
         val binding = FragmentPostsBinding.inflate(inflater, container, false)
 
         val adapter = PostsAdapter(object : OnPostInteractionListener {
-            override fun onLikePost(post: Post) {}
-            override fun onEditPost(post: Post) {}
-            override fun onRemovePost(post: Post) {}
-            override fun onSharePost(post: Post) {}
-            override fun onVideoPost(post: Post) {}
+            override fun onEditPost(post: Post) {
+                viewModel.editPost(post)
+                findNavController().navigate(R.id.action_postsFragment_to_addEditPostFragment,
+                Bundle().apply{
+                    textDataContent = post.content
+                    textDataVideo = post.videoAttach })
+            }
+
+            override fun onLikePost(post: Post) { viewModel.likedById(post.id) }
+            override fun onRemovePost(post: Post) { viewModel.removedById(post.id) }
+            override fun onSharePost(post: Post) { viewModel.sharedById(post.id) }
+            override fun onVideoPost(post: Post) {
+                post.videoAttach?.let {
+                    val intent = Intent().apply {
+                        action = Intent.ACTION_VIEW
+                        Intent(Intent.ACTION_VIEW, Uri.parse("url"))
+                        data = Uri.parse(post.videoAttach)
+                    }
+                    val videoIntent =
+                        Intent.createChooser(intent, getString(R.string.chooser_video_post))
+                    startActivity(videoIntent)
+                }
+            }
             override fun onShowPost(post: Post) {}
         })
 
         binding.rvListOfPosts.adapter = adapter
+        viewModel.data.observe(viewLifecycleOwner, { posts ->
+            adapter.submitList(posts)
+        })
+
+        binding.fab.setOnClickListener {
+            findNavController().navigate(R.id.action_postsFragment_to_addEditPostFragment)
+        }
 
         return binding.root
     }
