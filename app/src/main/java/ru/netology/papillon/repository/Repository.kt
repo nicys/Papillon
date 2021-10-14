ba0f2c961ab86fba4052a797f41b938f6e97629a
nicys <nicys2476@yandex.ru>
2021-10-14 15:27:10 +0700

    corrected Repository.kt

--- app/src/main/java/ru/netology/papillon/repository/Repository.kt
+++ app/src/main/java/ru/netology/papillon/repository/Repository.kt
@@ -1,12 +1,19 @@
 package ru.netology.papillon.repository
 
 import androidx.lifecycle.LiveData
+import ru.netology.papillon.dto.Job
 import ru.netology.papillon.dto.Post
 
 interface PostRepository {
-    fun getAll(): LiveData<List<Post>>
+    fun getAllPosts(): LiveData<List<Post>>
     fun likedById(id: Long)
     fun sharedById(id: Long)
     fun removedById(id: Long)
     fun savePost(post: Post)
+}
+
+interface JobRepository {
+    fun getAllJob(): LiveData<List<Job>>
+    fun removedById(id: Long)
+    fun saveJob(job: Job)
 }
