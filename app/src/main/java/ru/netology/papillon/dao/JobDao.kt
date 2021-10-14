c843e09321a35cd429040dbb45c97f0c0233cdf1
nicys <nicys2476@yandex.ru>
2021-10-14 13:24:01 +0700

    added JobDao.kt

--- app/src/main/java/ru/netology/papillon/dao/JobDao.kt
+++ app/src/main/java/ru/netology/papillon/dao/JobDao.kt
@@ -1,7 +1,20 @@
 package ru.netology.papillon.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import ru.netology.papillon.entity.JobEntity
 
 @Dao
 interface JobDao {
+    @Query("SELECT * FROM JobEntity ORDER BY id")
+    fun getAll(): LiveData<List<JobEntity>>
+
+    @Query("DELETE FROM JobEntity WHERE id = :id")
+    fun removedById(id: Long)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun saveJob(job: JobEntity): Long
 }
