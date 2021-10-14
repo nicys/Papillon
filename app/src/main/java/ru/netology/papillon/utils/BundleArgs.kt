d7f55e2788eab262048de4a03ba42dd5f61ae90a
nicys <nicys2476@yandex.ru>
2021-10-14 15:26:35 +0700

    added objects to BundleArgs.kt

--- app/src/main/java/ru/netology/papillon/utils/BundleArgs.kt
+++ app/src/main/java/ru/netology/papillon/utils/BundleArgs.kt
@@ -1,7 +1,9 @@
 package ru.netology.papillon.utils
 
 import android.os.Bundle
+import ru.netology.papillon.dto.Job
 import ru.netology.papillon.dto.Post
+import ru.netology.papillon.dto.User
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
@@ -11,6 +13,26 @@ object PostArg: ReadWriteProperty<Bundle, Post?> {
     }
 
     override fun getValue(thisRef: Bundle, property: KProperty<*>): Post? {
+        return thisRef.getParcelable(property.name)
+    }
+}
+
+object JobArg: ReadWriteProperty<Bundle, Job?> {
+    override fun setValue(thisRef: Bundle, property: KProperty<*>, value: Job?) {
+        thisRef.putParcelable(property.name, value)
+    }
+
+    override fun getValue(thisRef: Bundle, property: KProperty<*>): Job? {
+        return thisRef.getParcelable(property.name)
+    }
+}
+
+object UserArg: ReadWriteProperty<Bundle, User?> {
+    override fun setValue(thisRef: Bundle, property: KProperty<*>, value: User?) {
+        thisRef.putParcelable(property.name, value)
+    }
+
+    override fun getValue(thisRef: Bundle, property: KProperty<*>): User? {
         return thisRef.getParcelable(property.name)
     }
 }
