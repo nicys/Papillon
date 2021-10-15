fdbe529cc53801726164cbc9250f47a77c4e9289
nicys <nicys2476@yandex.ru>
2021-10-15 12:42:26 +0700

    corrected UserDao.kt

--- app/src/main/java/ru/netology/papillon/dao/UserDao.kt
+++ app/src/main/java/ru/netology/papillon/dao/UserDao.kt
@@ -9,12 +9,12 @@ import ru.netology.papillon.entity.UserEntity
 
 @Dao
 interface UserDao {
-    @Query("SELECT * FROM JobEntity ORDER BY id")
+    @Query("SELECT * FROM UserEntity ORDER BY idUser")
     fun getAll(): LiveData<List<UserEntity>>
 
-    @Query("DELETE FROM UserEntity WHERE idUser = :id")
-    fun removedById(id: Long)
+    @Query("DELETE FROM UserEntity WHERE idUser = :idUser")
+    fun removedById(idUser: Long)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun saveJob(user: UserEntity): Long
+    fun saveUser(user: UserEntity): Long
 }
