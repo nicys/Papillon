66ab8c09248a7dada8c314f634842714aa484639
nicys <nicys2476@yandex.ru>
2021-10-15 12:35:03 +0700

    added UserDao.kt

--- app/src/main/java/ru/netology/papillon/dao/UserDao.kt
+++ app/src/main/java/ru/netology/papillon/dao/UserDao.kt
@@ -1,7 +1,20 @@
 package ru.netology.papillon.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import ru.netology.papillon.entity.UserEntity
 
 @Dao
 interface UserDao {
+    @Query("SELECT * FROM JobEntity ORDER BY id")
+    fun getAll(): LiveData<List<UserEntity>>
+
+    @Query("DELETE FROM UserEntity WHERE idUser = :id")
+    fun removedById(id: Long)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun saveJob(user: UserEntity): Long
 }
