ef12a53feb923e898fd6f668cc8c95dbcde7348d
nicys <nicys2476@yandex.ru>
2021-10-15 12:47:56 +0700

    corrected name of property

--- app/src/main/java/ru/netology/papillon/entity/UserEntity.kt
+++ app/src/main/java/ru/netology/papillon/entity/UserEntity.kt
@@ -9,10 +9,10 @@ data class UserEntity(
     @PrimaryKey(autoGenerate = true)
     val idUser: Long,
     val avatar: String?,
-    val login: String,
+    val entered: String,
     val name: String,
     val surname: String,
     val isMe: Boolean,
 ) {
-    fun toDtoUser() = User(idUser, avatar, login, name, surname, isMe)
+    fun toDtoUser() = User(idUser, avatar, entered, name, surname, isMe)
 }
