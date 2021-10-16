6f264af0f12b94d50d6ed6a6280479f97bad72c2
nicys <nicys2476@yandex.ru>
2021-10-16 18:40:57 +0700

    initial AddEditUserFragment.kt

---
+++ app/src/main/java/ru/netology/papillon/AddEditUserFragment.kt
@@ -0,0 +1,20 @@
+package ru.netology.papillon
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AddEditUserFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_edit_user, container, false)
+    }
+
+
+}
