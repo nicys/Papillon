9d2942b73f9b67bf0b985792f8cc34bdc56ff359
nicys <nicys2476@yandex.ru>
2021-10-16 15:12:49 +0700

    corrected

--- app/src/main/java/ru/netology/papillon/AppActivity.kt
+++ app/src/main/java/ru/netology/papillon/AppActivity.kt
@@ -7,5 +7,6 @@ class AppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app)
+
     }
 }
