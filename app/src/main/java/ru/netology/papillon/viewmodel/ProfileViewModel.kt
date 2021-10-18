34759124488674716193313edcc74e527665608f
nicys <nicys2476@yandex.ru>
2021-10-18 12:50:14 +0700

    added ProfileViewModel.kt

--- app/src/main/java/ru/netology/papillon/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/ru/netology/papillon/viewmodel/ProfileViewModel.kt
@@ -1,2 +1,41 @@
 package ru.netology.papillon.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import ru.netology.papillon.db.AppDbUser
+import ru.netology.papillon.dto.User
+import ru.netology.papillon.repository.UserRepository
+import ru.netology.papillon.repository.UserRepositoryImpl
+
+private val empty = User()
+
+class ProfileViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val userRepository: UserRepository = UserRepositoryImpl(
+        AppDbUser.getInstance(context = application).usersDao()
+    )
+//    val data = userRepository.getAllUsers()
+    val edited = MutableLiveData(empty)
+
+    fun saveUser() {
+        edited.value?.let {
+            userRepository.saveUser(it)
+        }
+        edited.value = empty
+    }
+
+    fun editName(user: User) {
+        edited.value = user
+    }
+
+    fun changeName(name: String) {
+        val text = name.trim()
+        if (edited.value?.name == text) {
+            return
+        }
+        edited.value = edited.value?.copy(name = text)
+    }
+
+    fun removedById(idUser: Long) = userRepository.removedById(idUser)
+}
