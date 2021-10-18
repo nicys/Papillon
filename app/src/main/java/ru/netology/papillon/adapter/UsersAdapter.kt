2c30e06d74b250ae0b9fbed39ddccf116e5678a2
nicys <nicys2476@yandex.ru>
2021-10-18 12:14:37 +0700

    added codes to UsersAdapter.kt

--- app/src/main/java/ru/netology/papillon/adapter/UsersAdapter.kt
+++ app/src/main/java/ru/netology/papillon/adapter/UsersAdapter.kt
@@ -3,13 +3,17 @@ package ru.netology.papillon.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import ru.netology.papillon.R
 import ru.netology.papillon.databinding.CardUserBinding
 import ru.netology.papillon.dto.User
 
 interface OnUserInteractionListener {
+    fun onEditUser(user: User)
+    fun oDeleteUser(user: User)
 //    fun onUserClick(user: User)
 }
 
@@ -45,6 +49,24 @@ class UsersViewHolder(
     fun bind(user: User) {
         with(binding) {
             tvUserName.text = user.name
+            mbPopupMenu.setOnClickListener {
+                PopupMenu(it.context, it).apply {
+                    inflate(R.menu.user_options_menu)
+                    setOnMenuItemClickListener { item ->
+                        when(item.itemId) {
+                            R.id.delete -> {
+                                onUserInteractionListener.oDeleteUser(user)
+                                true
+                            }
+                            R.id.edit -> {
+                                onUserInteractionListener.onEditUser(user)
+                                true
+                            }
+                            else -> false
+                        }
+                    }
+                }.show()
+            }
 
 //            cvIsYou.visibility = if (user.isMe) View.VISIBLE else View.GONE
         }
