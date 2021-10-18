050cd7c80b0875a71f2bd8c41ee8d3e4e18b8624
nicys <nicys2476@yandex.ru>
2021-10-18 12:34:58 +0700

    added codes for adapter

--- app/src/main/java/ru/netology/papillon/UsersFragment.kt
+++ app/src/main/java/ru/netology/papillon/UsersFragment.kt
@@ -6,10 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
+import ru.netology.papillon.AddEditUserFragment.Companion.textUserName
 import ru.netology.papillon.adapter.OnUserInteractionListener
 import ru.netology.papillon.adapter.UsersAdapter
 import ru.netology.papillon.databinding.FragmentUsersBinding
+import ru.netology.papillon.dto.User
 import ru.netology.papillon.viewmodel.UserViewModel
 
 class UsersFragment : Fragment() {
@@ -24,10 +27,19 @@ class UsersFragment : Fragment() {
         val binding = FragmentUsersBinding.inflate(inflater, container, false)
 
         val adapter = UsersAdapter(object : OnUserInteractionListener {
+            override fun onEditUser(user: User) {
+                viewModel.editName(user)
+                findNavController().navigate(R.id.action_profileFragment_to_addEditUserFragment,
+                    Bundle().apply {
+                        textUserName = user.name
+                    })
+            }
+            override fun oDeleteUser(user: User) {
+                viewModel.removedById(user.idUser)
+            }
 //            override fun onUserClick(user: User) {
 //                TODO("Not yet implemented")
 //            }
-
         })
 
         binding.rvListOfUsers.adapter = adapter
