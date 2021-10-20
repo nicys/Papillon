169754a6183def7ba87e413bea4cceb6d375fe96
nicys <nicys2476@yandex.ru>
2021-10-20 13:10:21 +0700

    added AddEditProfileFragment.kt

--- app/src/main/java/ru/netology/papillon/AddEditProfileFragment.kt
+++ app/src/main/java/ru/netology/papillon/AddEditProfileFragment.kt
@@ -5,55 +5,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import ru.netology.papillon.databinding.FragmentAddEditProfileBinding
+import ru.netology.papillon.databinding.FragmentAddEditUserBinding
+import ru.netology.papillon.utils.AndroidUtils
+import ru.netology.papillon.utils.StringArg
+import ru.netology.papillon.viewmodel.UserViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AddEditProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AddEditProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    companion object {
+        var Bundle.textUserName: String? by StringArg
     }
+
+    private val viewModel: UserViewModel by viewModels(
+        ownerProducer = ::requireParentFragment)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_edit_profile, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddEditProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddEditProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val binding = FragmentAddEditProfileBinding.inflate(inflater, container, false)
+
+        binding.etUserName.requestFocus()
+        AndroidUtils.showKeyboard(binding.root)
+
+        arguments?.textUserName?.let { binding.etUserName.setText(it) }
+
+        binding.btConfirm.setOnClickListener {
+            viewModel.changeName(binding.etUserName.text.toString())
+            viewModel.saveUser()
+            AndroidUtils.hideKeyboard(requireView())
+            findNavController().navigateUp()
+        }
+
+        binding.btCancel.setOnClickListener {
+            AndroidUtils.hideKeyboard(requireView())
+            findNavController().navigateUp()
+        }
+
+        return binding.root
     }
 }
