0c389970220a1809c4b77df65114ecc789a04868
nicys <nicys2476@yandex.ru>
2021-11-01 15:02:53 +0700

    UserRepositoryImpl.kt remade on suspend + Retrofit

--- app/src/main/java/ru/netology/papillon/repository/UserRepositoryImpl.kt
+++ app/src/main/java/ru/netology/papillon/repository/UserRepositoryImpl.kt
@@ -1,25 +1,63 @@
 package ru.netology.papillon.repository
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Transformations
+import androidx.lifecycle.map
+import ru.netology.papillon.api.Api
 import ru.netology.papillon.dao.UserDao
 import ru.netology.papillon.dto.User
+import ru.netology.papillon.entity.UserEntity
+import ru.netology.papillon.error.ApiError
+import ru.netology.papillon.error.NetworkError
+import ru.netology.papillon.error.UnknownError
+import ru.netology.papillon.extensions.toDtoUser
 import ru.netology.papillon.extensions.toEntityUser
+import java.io.IOException
 
-class UserRepositoryImpl(
-    private val userDao: UserDao
-) : UserRepository {
-    override fun getAllUsers() = Transformations.map(userDao.getAll()) {
-        it.map {
-            User(it.idUser, it.avatar, it.entered, it.name, it.surname, it.isMe)
+class UserRepositoryImpl(private val userDao: UserDao) : Repository<User> {
+
+    override val data = userDao.getAll().map(List<UserEntity>::toDtoUser)
+
+    override suspend fun getAll() {
+        try {
+            val response = Api.service.getAllUsers()
+            if (!response.isSuccessful) {
+                throw ApiError(response.code(), response.message())
+            }
+            val body = response.body() ?: throw ApiError(response.code(), response.message())
+            userDao.insert(body.toEntityUser())
+        } catch (e: IOException) {
+            NetworkError
+        } catch (e: Exception) {
+            UnknownError
         }
     }
 
-    override fun removedById(idUser: Long) {
-        userDao.removedById(idUser)
+    override suspend fun save(t: User) {
+        try {
+            val response = Api.service.saveUser(t)
+            if (!response.isSuccessful) {
+                throw ApiError(response.code(), response.message())
+            }
+            val body = response.body() ?: throw ApiError(response.code(), response.message())
+            userDao.insert(UserEntity.fromDtoUser(body))
+        } catch (e: IOException) {
+            NetworkError
+        } catch (e: Exception) {
+            UnknownError
+        }
     }
 
-    override fun saveUser(user: User) {
-        userDao.saveUser(user.toEntityUser())
+    override suspend fun removedById(id: Long) {
+        try {
+            val response = Api.service.removedByIdUser(id)
+            if (!response.isSuccessful) {
+                throw ApiError(response.code(), response.message())
+            }
+            response.body() ?: throw ApiError(response.code(), response.message())
+            userDao.removedById(id)
+        } catch (e: IOException) {
+            NetworkError
+        } catch (e: Exception) {
+            UnknownError
+        }
     }
 }
