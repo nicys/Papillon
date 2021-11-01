33374f2c932fe09ba9e91f4d4de824d07cb5bec8
nicys <nicys2476@yandex.ru>
2021-11-01 12:05:37 +0700

    corrected FeedModel.kt

--- app/src/main/java/ru/netology/papillon/model/FeedModel.kt
+++ app/src/main/java/ru/netology/papillon/model/FeedModel.kt
@@ -2,10 +2,8 @@ package ru.netology.papillon.model
 
 import ru.netology.papillon.dto.Post
 
+//Отвечает непосредственно за данные (список постов)
 data class FeedModel(
     val posts: List<Post> = emptyList(),
-    val loading: Boolean = false,
-    val error: Boolean = false,
     val empty: Boolean = false,
-    val refreshing: Boolean = false,
 )
