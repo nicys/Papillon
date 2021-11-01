6cdb9e1b72e4de7d6872bd97b3c9d3029c254eb5
nicys <nicys2476@yandex.ru>
2021-11-01 14:15:18 +0700

    PostRepositoryImpl.kt remade on suspend + Retrofit

--- app/src/main/java/ru/netology/papillon/repository/PostRepositoryImpl.kt
+++ app/src/main/java/ru/netology/papillon/repository/PostRepositoryImpl.kt
@@ -1,42 +1,108 @@
 package ru.netology.papillon.repository
 
-import androidx.lifecycle.Transformations
+import androidx.lifecycle.map
+import ru.netology.papillon.api.Api
 import ru.netology.papillon.dao.PostDao
 import ru.netology.papillon.dto.Post
 import ru.netology.papillon.entity.PostEntity
-//import ru.netology.papillon.extensions.toDtoPost
-//import ru.netology.papillon.extensions.toEntityPost
+import ru.netology.papillon.error.ApiError
+import ru.netology.papillon.error.NetworkError
+import ru.netology.papillon.error.UnknownError
+import ru.netology.papillon.extensions.toDtoPost
+import ru.netology.papillon.extensions.toEntityPost
+import java.io.IOException
 
-class PostRepositoryImpl(
-    private val postDao: PostDao
-) : PostRepository {
-    override fun getAllPosts() = Transformations.map(postDao.getAll()) {
-        it.map {
-            Post(
-                it.id, it.authorId, it.author, it.authorAvatar, it.published,
-                it.content, it.likedByMe, it.likesCnt, it.shares, it.sharesCnt,
-                it.views, it.viewsCnt, it.videoAttach, it.audioAttach, it.imageAttach
-            )
+class PostRepositoryImpl(private val postDao: PostDao) : PostRepository {
+    override val data = postDao.getAll().map(List<PostEntity>::toDtoPost)
+
+    override suspend fun getAll() {
+        try {
+            val response = Api.service.getAllPosts()
+            if (!response.isSuccessful) {
+                throw ApiError(response.code(), response.message())
+            }
+            val body = response.body() ?: throw ApiError(response.code(), response.message())
+            postDao.insert(body.toEntityPost())
+        } catch (e: IOException) {
+            throw NetworkError
+        } catch (e: Exception) {
+            throw UnknownError
         }
     }
 
-    override fun likedById(id: Long) {
-        postDao.likedById(id)
+    override suspend fun save(post: Post) {
+        try {
+            val response = Api.service.savePost(post)
+            if (!response.isSuccessful) {
+                throw ApiError(response.code(), response.message())
+            }
+            val body = response.body() ?: throw ApiError(response.code(), response.message())
+            postDao.insert(PostEntity.fromDtoPost(body))
+        } catch (e: IOException) {
+            throw NetworkError
+        } catch (e: Exception) {
+            throw UnknownError
+        }
     }
 
-    override fun sharedById(id: Long) {
-        postDao.sharedById(id)
+    override suspend fun likedById(id: Long) {
+        try {
+            val response = Api.service.likedByIdPost(id)
+            if (!response.isSuccessful) {
+                throw ApiError(response.code(), response.message())
+            }
+            response.body() ?: throw ApiError(response.code(), response.message())
+            postDao.likedById(id)
+        } catch (e: IOException) {
+            throw NetworkError
+        } catch (e: Exception) {
+            throw UnknownError
+        }
     }
 
-    override fun viewedById(id: Long) {
-        postDao.viewedById(id)
+    override suspend fun sharedById(id: Long) {
+        try {
+            val response = Api.service.sharedByIdPost(id)
+            if (!response.isSuccessful) {
+                throw ApiError(response.code(), response.message())
+            }
+            response.body() ?: throw ApiError(response.code(), response.message())
+            postDao.sharedById(id)
+        } catch (e: IOException) {
+            throw NetworkError
+        } catch (e: Exception) {
+            throw UnknownError
+        }
+
     }
 
-    override fun removedById(id: Long) {
-        postDao.removedById(id)
+    override suspend fun viewedById(id: Long) {
+        try {
+            val response = Api.service.viewedByIdPost(id)
+            if (!response.isSuccessful) {
+                throw ApiError(response.code(), response.message())
+            }
+            response.body() ?: throw ApiError(response.code(), response.message())
+            postDao.viewedById(id)
+        } catch (e: IOException) {
+            NetworkError
+        } catch (e: Exception) {
+            UnknownError
+        }
     }
 
-    override fun savePost(post: Post) {
-        postDao.savePost(PostEntity.fromDtoPost(post))
+    override suspend fun removedById(id: Long) {
+        try {
+            val response = Api.service.removedByIdPost(id)
+            if (!response.isSuccessful) {
+                throw ApiError(response.code(), response.message())
+            }
+            response.body() ?: throw ApiError(response.code(), response.message())
+            postDao.removedById(id)
+        } catch (e: IOException) {
+            NetworkError
+        } catch (e: Exception) {
+            UnknownError
+        }
     }
 }
