a1825f5979673901be0626347d53eba6355d2bdd
nicys <nicys2476@yandex.ru>
2021-11-01 15:03:30 +0700

    corrected code's in Repository.kt

--- app/src/main/java/ru/netology/papillon/repository/Repository.kt
+++ app/src/main/java/ru/netology/papillon/repository/Repository.kt
@@ -1,29 +1,21 @@
 package ru.netology.papillon.repository
 
 import androidx.lifecycle.LiveData
-import ru.netology.papillon.dto.Job
 import ru.netology.papillon.dto.Post
-import ru.netology.papillon.dto.User
 
 interface PostRepository {
     val data: LiveData<List<Post>>
-    fun getAll()
-    fun likedById(id: Long)
-    fun sharedById(id: Long)
-    fun viewedById(id: Long)
-    fun removedById(id: Long)
-    fun save(post: Post)
+    suspend fun getAll()
+    suspend fun likedById(id: Long)
+    suspend fun sharedById(id: Long)
+    suspend fun viewedById(id: Long)
+    suspend fun removedById(id: Long)
+    suspend fun save(post: Post)
 }
 
 interface Repository<T> {
     val data: LiveData<List<T>>
-    fun getAll(): LiveData<List<T>>
-    fun removedById(id: Long)
-    fun save(t: T)
+    suspend fun getAll()
+    suspend fun removedById(id: Long)
+    suspend fun save(t: T)
 }
-
-//interface UserRepository {
-//    fun getAllUsers(): LiveData<List<User>>
-//    fun removedById(id: Long)
-//    fun saveUser(user: User)
-//}
