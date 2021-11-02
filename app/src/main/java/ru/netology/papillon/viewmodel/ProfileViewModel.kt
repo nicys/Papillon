70266c86ad06d9bd3d73874cdbeae01e913819d3
nicys <nicys2476@yandex.ru>
2021-11-02 15:42:40 +0700

    commented code's at ProfileViewModel.kt

--- app/src/main/java/ru/netology/papillon/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/ru/netology/papillon/viewmodel/ProfileViewModel.kt
@@ -6,49 +6,47 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.map
 import ru.netology.papillon.db.AppDbUser
+import ru.netology.papillon.dto.Post
 import ru.netology.papillon.dto.User
-import ru.netology.papillon.repository.UserRepository
+import ru.netology.papillon.repository.Repository
 import ru.netology.papillon.repository.UserRepositoryImpl
 
 private val empty = User()
 
 class ProfileViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val userRepository: UserRepository = UserRepositoryImpl(
-        AppDbUser.getInstance(context = application).usersDao()
-    )
+    private val userRepository: Repository<User> =
+        UserRepositoryImpl(AppDbUser.getInstance(context = application).usersDao())
 
-    private val _currentUser = MutableLiveData(User())
-    val currentUser: LiveData<User>
-        get() = _currentUser
+    val dataUser = userRepository.data
 
-    val data = userRepository.getAllUsers()
-    val edited = MutableLiveData(empty)
-
-    fun saveUser() {
-        edited.value?.let {
-            userRepository.saveUser(it)
-        }
-        edited.value = empty
-    }
-
-    fun editName(user: User) {
-        edited.value = user
-    }
-
-    fun changeName(name: String) {
-        val text = name.trim()
-        if (edited.value?.name == text) {
-            return
-        }
-        edited.value = edited.value?.copy(name = text)
-    }
-
-    fun removedById(idUser: Long) = userRepository.removedById(idUser)
-
-    fun getUserById(id: Long): LiveData<User?> = data.map { users ->
-        users.find {
-            it.idUser == id
-        }
-    }
+    //    private val _currentUser = MutableLiveData(User())
+//    val currentUser: LiveData<User>
+//        get() = _currentUser
+//
+//    val data = userRepository.getAllUsers()
+//
+//    val edited = MutableLiveData(empty)
+//
+//    fun saveUser() {
+//        edited.value?.let {
+//            userRepository.saveUser(it)
+//        }
+//        edited.value = empty
+//    }
+//
+//    fun editName(user: User) {
+//        edited.value = user
+//    }
+//
+//    fun changeName(name: String) {
+//        val text = name.trim()
+//        if (edited.value?.name == text) {
+//            return
+//        }
+//        edited.value = edited.value?.copy(name = text)
+//    }
+//
+//    fun removedById(idUser: Long) = userRepository.removedById(idUser)
+//
 }
