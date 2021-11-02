6573a6a5c2f980511d6a11aa0ae343792c09e47b
nicys <nicys2476@yandex.ru>
2021-11-02 13:57:10 +0700

    added data class's at FeedModel.kt

--- app/src/main/java/ru/netology/papillon/model/FeedModel.kt
+++ app/src/main/java/ru/netology/papillon/model/FeedModel.kt
@@ -1,9 +1,21 @@
 package ru.netology.papillon.model
 
+import ru.netology.papillon.dto.Job
 import ru.netology.papillon.dto.Post
+import ru.netology.papillon.dto.User
 
-//Отвечает непосредственно за данные (список постов)
-data class FeedModel(
+//Отвечают непосредственно за данные (список постов, работу и пользователей)
+data class FeedModelPosts(
     val posts: List<Post> = emptyList(),
+    val empty: Boolean = false,
+)
+
+data class FeedModelJobs(
+    val jobs: List<Job> = emptyList(),
+    val empty: Boolean = false,
+)
+
+data class FeedModelUsers(
+    val users: List<User> = emptyList(),
     val empty: Boolean = false,
 )
