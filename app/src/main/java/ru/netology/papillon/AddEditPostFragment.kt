abe5566443ad316bb9173520b5b55f2ffcc2c5ba
nicys <nicys2476@yandex.ru>
2021-11-04 14:45:57 +0700

    added code's at AddEditPostFragment.kt

--- app/src/main/java/ru/netology/papillon/AddEditPostFragment.kt
+++ app/src/main/java/ru/netology/papillon/AddEditPostFragment.kt
@@ -1,10 +1,17 @@
 package ru.netology.papillon
 
+import android.app.Activity
+import android.net.Uri
 import android.os.Bundle
 import android.view.*
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.net.toFile
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.github.dhaval2404.imagepicker.ImagePicker
+import com.github.dhaval2404.imagepicker.constant.ImageProvider
+import com.google.android.material.snackbar.Snackbar
 import ru.netology.papillon.databinding.FragmentAddEditPostBinding
 import ru.netology.papillon.utils.AndroidUtils
 import ru.netology.papillon.utils.AndroidUtils.hideKeyboard
@@ -52,22 +59,75 @@ class AddEditPostFragment : Fragment() {
     ): View? {
         val binding = FragmentAddEditPostBinding.inflate(inflater, container, false)
 
+        fragmentBinding = binding
+
+        arguments?.textDataContent
+            ?.let(binding.etEdit::setText)
+
         binding.etEdit.requestFocus()
-        showKeyboard(binding.root)
 
-        arguments?.textDataContent?.let { binding.etEdit.setText(it) }
+        val pickPhotoLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                when (it.resultCode) {
+                    ImagePicker.RESULT_ERROR -> {
+                        Snackbar.make(
+                            binding.root,
+                            ImagePicker.getError(it.data),
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+                    Activity.RESULT_OK -> {
+                        val uri: Uri? = it.data?.data
+                        postViewModel.changePhoto(uri, uri?.toFile())
+                    }
+                }
+            }
 
-//        binding.ok.setOnClickListener {
-//            postViewModel.changeContent(binding.etEditContent.text.toString())
-//            postViewModel.savePost()
-//            hideKeyboard(requireView())
-//        }
+        binding.pickPhoto.setOnClickListener {
+            ImagePicker.with(this)
+                .crop()
+                .compress(2048)
+                .provider(ImageProvider.GALLERY)
+                .galleryMimeTypes(
+                    arrayOf(
+                        "image/png",
+                        "image/jpeg",
+                    )
+                )
+                .createIntent(pickPhotoLauncher::launch)
+        }
+
+        binding.takePhoto.setOnClickListener {
+            ImagePicker.with(this)
+                .crop()
+                .compress(2048)
+                .provider(ImageProvider.CAMERA)
+                .createIntent(pickPhotoLauncher::launch)
+        }
+
+        binding.removePhoto.setOnClickListener {
+            postViewModel.changePhoto(null, null)
+        }
 
         postViewModel.postCreated.observe(viewLifecycleOwner) {
-            postViewModel.loadPosts()
             findNavController().navigateUp()
         }
 
+        postViewModel.photo.observe(viewLifecycleOwner) {
+            if (it.uri == null) {
+                binding.photoContainer.visibility = View.GONE
+                return@observe
+            }
+
+            binding.photoContainer.visibility = View.VISIBLE
+            binding.photo.setImageURI(it.uri)
+        }
+
         return binding.root
+    }
+
+    override fun onDestroyView() {
+        fragmentBinding = null
+        super.onDestroyView()
     }
 }
