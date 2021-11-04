82167398c47724fa7783f4f47cc089eb4c9c911c
nicys <nicys2476@yandex.ru>
2021-11-04 12:47:04 +0700

    added codes's at AddEditPostFragment.kt

--- app/src/main/java/ru/netology/papillon/AddEditPostFragment.kt
+++ app/src/main/java/ru/netology/papillon/AddEditPostFragment.kt
@@ -1,13 +1,12 @@
 package ru.netology.papillon
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import ru.netology.papillon.databinding.FragmentAddEditPostBinding
+import ru.netology.papillon.utils.AndroidUtils
 import ru.netology.papillon.utils.AndroidUtils.hideKeyboard
 import ru.netology.papillon.utils.AndroidUtils.showKeyboard
 import ru.netology.papillon.utils.StringArg
@@ -22,22 +21,47 @@ class AddEditPostFragment : Fragment() {
     private val postViewModel: PostViewModel by viewModels(
         ownerProducer = ::requireParentFragment)
 
+    private var fragmentBinding: FragmentAddEditPostBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_edit_post_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.save -> {
+                fragmentBinding?.let {
+                    postViewModel.changeContent(it.etEdit.text.toString())
+                    postViewModel.savePost()
+                    hideKeyboard(requireView())
+                }
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentAddEditPostBinding.inflate(inflater, container, false)
 
-        binding.etEditContent.requestFocus()
+        binding.etEdit.requestFocus()
         showKeyboard(binding.root)
 
-        arguments?.textDataContent?.let { binding.etEditContent.setText(it) }
+        arguments?.textDataContent?.let { binding.etEdit.setText(it) }
 
-        binding.ok.setOnClickListener {
-            postViewModel.changeContent(binding.etEditContent.text.toString())
-            postViewModel.savePost()
-            hideKeyboard(requireView())
-        }
+//        binding.ok.setOnClickListener {
+//            postViewModel.changeContent(binding.etEditContent.text.toString())
+//            postViewModel.savePost()
+//            hideKeyboard(requireView())
+//        }
 
         postViewModel.postCreated.observe(viewLifecycleOwner) {
             postViewModel.loadPosts()
