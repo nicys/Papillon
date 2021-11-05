d074ab32c4b2b403ecdc349dea1fc25034a98c21
nicys <nicys2476@yandex.ru>
2021-11-05 16:16:17 +0700

    corrected ViewExtensions.kt

---
+++ app/src/main/java/ru/netology/papillon/extensions/ViewExtensions.kt
@@ -0,0 +1,19 @@
+package ru.netology.papillon.extensions
+
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.BitmapTransformation
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import ru.netology.papillon.R
+
+fun ImageView.load(url: String, vararg transforms: BitmapTransformation = emptyArray()) =
+    Glide.with(this)
+        .load(url)
+        .timeout(10_000)
+        .placeholder(R.drawable.ic_no_avatar_user)
+        .error(R.drawable.ic_avatar_error_foreground)
+        .transform(*transforms)
+        .into(this)
+
+fun ImageView.loadCircleCrop(url: String, vararg transforms: BitmapTransformation = emptyArray()) =
+    load(url, CircleCrop(), *transforms)
