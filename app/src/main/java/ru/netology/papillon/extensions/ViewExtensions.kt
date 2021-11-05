1bede9673fe0eaa1bc75acb5dfb25f3dc05eae4d
nicys <nicys2476@yandex.ru>
2021-11-05 18:03:02 +0700

    added fun ImageView.loadImage at ViewExtensions.kt

--- app/src/main/java/ru/netology/papillon/extensions/ViewExtensions.kt
+++ app/src/main/java/ru/netology/papillon/extensions/ViewExtensions.kt
@@ -1,12 +1,13 @@
 package ru.netology.papillon.extensions
 
 import android.widget.ImageView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.BitmapTransformation
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import ru.netology.papillon.R
 
-fun ImageView.load(url: String, vararg transforms: BitmapTransformation = emptyArray()) =
+fun ImageView.loadAvatars(url: String, vararg transforms: BitmapTransformation = emptyArray()) =
     Glide.with(this)
         .load(url)
         .timeout(10_000)
@@ -15,5 +16,18 @@ fun ImageView.load(url: String, vararg transforms: BitmapTransformation = emptyA
         .transform(*transforms)
         .into(this)
 
+fun ImageView.loadImage(url: String, vararg transforms: BitmapTransformation = emptyArray()) =
+    Glide.with(this)
+        .load(url)
+        .timeout(10_000)
+        .placeholder(CircularProgressDrawable(this.context).apply {
+            strokeWidth = 5f
+            centerRadius = 30f
+            start()
+        })
+        .error(R.drawable.ic_broken_image_24)
+        .transform(*transforms)
+        .into(this)
+
 fun ImageView.loadCircleCrop(url: String, vararg transforms: BitmapTransformation = emptyArray()) =
-    load(url, CircleCrop(), *transforms)
+    loadAvatars(url, CircleCrop(), *transforms)
