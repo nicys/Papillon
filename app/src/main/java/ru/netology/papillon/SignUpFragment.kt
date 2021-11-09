0a330d56130dc8df4026d23797b2624172c837a6
nicys <nicys2476@yandex.ru>
2021-11-09 12:55:14 +0700

    added SignUpFragment.kt

--- app/src/main/java/ru/netology/papillon/SignUpFragment.kt
+++ app/src/main/java/ru/netology/papillon/SignUpFragment.kt
@@ -1,31 +1,59 @@
 package ru.netology.papillon
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import ru.netology.papillon.databinding.FragmentSignUpBinding
+import ru.netology.papillon.utils.AndroidUtils.hideKeyboard
+import ru.netology.papillon.viewmodel.AuthViewModel
 
 
 class SignUpFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    @ExperimentalCoroutinesApi
+    private val authViewModel: AuthViewModel by viewModels(
+        ownerProducer = ::requireParentFragment
+    )
 
+    @ExperimentalCoroutinesApi
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
-    }
+        val binding = FragmentSignUpBinding.inflate(inflater, container, false)
 
+        binding.register.setOnClickListener {
+            val userName: String? = binding.inputName.text.toString()
+            val login: String? = binding.inputLogin2.text.toString()
+            val password: String? = binding.inputPassword2.text.toString()
+            if (userName == null || login == null || password == null) {
+                Snackbar.make(requireView(), getString(R.string.dontFilled), Snackbar.LENGTH_LONG)
+                    .show()
+                return@setOnClickListener
+            } else {
+                authViewModel.registration(userName, login, password)
+                hideKeyboard(it)
+                findNavController().navigateUp()
+            }
+
+        }
+
+        binding.toSingIn.setOnClickListener {
+            findNavController().navigate(R.id.action_signUpFragment_to_signInFragment)
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(this) {
+            findNavController().navigateUp()
+        }
+
+        return binding.root
+    }
 }
