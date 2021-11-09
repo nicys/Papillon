9b09315e3860e7bf58177ad7190969213e343905
nicys <nicys2476@yandex.ru>
2021-11-09 12:55:01 +0700

    added SignInFragment.kt

--- app/src/main/java/ru/netology/papillon/SignInFragment.kt
+++ app/src/main/java/ru/netology/papillon/SignInFragment.kt
@@ -1,59 +1,58 @@
 package ru.netology.papillon
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import ru.netology.papillon.databinding.FragmentSignInBinding
+import ru.netology.papillon.utils.AndroidUtils.hideKeyboard
+import ru.netology.papillon.viewmodel.AuthViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SignInFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SignInFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    @ExperimentalCoroutinesApi
+    private val authViewModel: AuthViewModel by viewModels(
+        ownerProducer = ::requireParentFragment
+    )
 
+    @ExperimentalCoroutinesApi
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false)
-    }
+        val binding = FragmentSignInBinding.inflate(inflater, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SignInFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SignInFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        binding.entrance.setOnClickListener {
+            val login: String? = binding.inputLogin.text.toString()
+            val password: String? = binding.inputPassword.text.toString()
+            if (login == null || password == null) {
+                Snackbar.make(binding.root, getString(R.string.dontFilled), Snackbar.LENGTH_LONG)
+                    .show()
+                return@setOnClickListener
+            } else {
+                authViewModel.authentication(login, password)
+                hideKeyboard(it)
+                findNavController().navigateUp()
             }
+
+        }
+
+        binding.toSingUp.setOnClickListener {
+            findNavController().navigate(R.id.action_signInFragment_to_signUpFragment)
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(this) {
+            findNavController().navigateUp()
+        }
+
+        return binding.root
     }
 }
