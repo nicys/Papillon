faaed9d60f594dcde7b984a450b920e2cc2f7d50
nicys <nicys2476@yandex.ru>
2021-11-11 19:51:41 +0700

    added PhotoImageFragment.kt

--- app/src/main/java/ru/netology/papillon/PhotoImageFragment.kt
+++ app/src/main/java/ru/netology/papillon/PhotoImageFragment.kt
@@ -5,55 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import ru.netology.papillon.databinding.FragmentPhotoImageBinding
+import ru.netology.papillon.dto.Post
+import ru.netology.papillon.extensions.loadImage
+import ru.netology.papillon.utils.PostArg
+import ru.netology.papillon.utils.StringArg
+import ru.netology.papillon.viewmodel.PostViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PhotoImageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PhotoImageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_photo_image, container, false)
-    }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PhotoImageFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PhotoImageFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        var Bundle.postPhoto: String? by StringArg
+        var Bundle.postData: Post? by PostArg
+    }
+
+    private val postViewModel: PostViewModel by viewModels(
+        ownerProducer = ::requireParentFragment
+    )
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentPhotoImageBinding.inflate(inflater, container, false)
+
+        with(postViewModel) {
+            arguments?.postData?.let {
+                val postWithPhoto = it
+                getPostById(postWithPhoto.id).observe(viewLifecycleOwner, { post ->
+                    post ?: return@observe
+                })
+                arguments?.postPhoto?.let {
+                    binding.fullScreenPhoto.loadImage("${BuildConfig.BASE_URL}/media/${postWithPhoto.attachment?.url}")
                 }
             }
+            return binding.root
+        }
     }
 }
