65585cb3f4ab61c219fa7f3c6f31970935371f07
nicys <nicys2476@yandex.ru>
2021-11-11 20:23:17 +0700

    initial enum class AttachmentType

--- app/src/main/java/ru/netology/papillon/dto/Media.kt
+++ app/src/main/java/ru/netology/papillon/dto/Media.kt
@@ -5,7 +5,3 @@ import java.io.File
 data class Media(val id: String)
 
 data class MediaUpload(val file: File)
-
-enum class AttachmentType{
-    IMAGE, VIDEO, AUDIO
-}
---
+++ app/src/main/java/ru/netology/papillon/enumeration/AttachmentType.kt
@@ -0,0 +1,5 @@
+package ru.netology.papillon.enumeration
+
+enum class AttachmentType{
+    IMAGE, VIDEO, AUDIO
+}
