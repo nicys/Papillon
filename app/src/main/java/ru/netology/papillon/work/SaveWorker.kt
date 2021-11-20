f6f4b36693297e258a571bdd461464a0a3f7fa3c
nicys <nicys2476@yandex.ru>
2021-11-20 18:40:57 +0700

    commented SaveWorker.kt

--- app/src/main/java/ru/netology/papillon/work/SaveWorker.kt
+++ app/src/main/java/ru/netology/papillon/work/SaveWorker.kt
@@ -10,83 +10,83 @@ import ru.netology.papillon.dto.Job
 import ru.netology.papillon.dto.User
 import ru.netology.papillon.repository.*
 
-class SavePostWorker(
-    applicationContext: Context,
-    params: WorkerParameters
-) : CoroutineWorker(applicationContext, params) {
-    companion object {
-        const val postKey = "post"
-    }
-
-    override suspend fun doWork(): Result {
-        val id = inputData.getLong(postKey, 0L)
-        if (id == 0L) {
-            return Result.failure()
-        }
-        val repository: PostRepository =
-            PostRepositoryImpl(
-                AppDbPost.getInstance(context = applicationContext).postsDao(),
-                AppDbPost.getInstance(context = applicationContext).postsWorkDao(),
-            )
-        return try {
-            repository.processWork(id)
-            Result.success()
-        } catch (e: Exception) {
-            Result.retry()
-        }
-    }
-}
-
-class SaveJobWorker(
-    applicationContext: Context,
-    params: WorkerParameters
-) : CoroutineWorker(applicationContext, params) {
-    companion object {
-        const val jobKey = "job"
-    }
-
-    override suspend fun doWork(): Result {
-        val id = inputData.getLong(jobKey, 0L)
-        if (id == 0L) {
-            return Result.failure()
-        }
-        val repository: Repository<Job> =
-            JobRepositoryImpl(
-                AppDbJob.getInstance(context = applicationContext).jobsDao(),
-                AppDbJob.getInstance(context = applicationContext).jobsWorkDao(),
-            )
-        return try {
-            repository.processWork(id)
-            Result.success()
-        } catch (e: Exception) {
-            Result.retry()
-        }
-    }
-}
-
-class SaveUserWorker(
-    applicationContext: Context,
-    params: WorkerParameters
-) : CoroutineWorker(applicationContext, params) {
-    companion object {
-        const val userKey = "user"
-    }
-
-    override suspend fun doWork(): Result {
-        val id = inputData.getLong(userKey, 0L)
-        if (id == 0L) {
-            return Result.failure()
-        }
-        val repository: Repository<User> =
-            UserRepositoryImpl(
-                AppDbUser.getInstance(context = applicationContext).usersDao(),
-                AppDbUser.getInstance(context = applicationContext).usersWorkDao(),
-            )
-        return try {
-            repository.processWork(id)
-            Result.success()
-        } catch (e: Exception) {
-            Result.retry()
-        }
-    }
-}
+//class SavePostWorker(
+//    applicationContext: Context,
+//    params: WorkerParameters
+//) : CoroutineWorker(applicationContext, params) {
+//    companion object {
+//        const val postKey = "post"
+//    }
+//
+//    override suspend fun doWork(): Result {
+//        val id = inputData.getLong(postKey, 0L)
+//        if (id == 0L) {
+//            return Result.failure()
+//        }
+//        val repository: PostRepository =
+//            PostRepositoryImpl(
+//                AppDbPost.getInstance(context = applicationContext).postsDao(),
+//                AppDbPost.getInstance(context = applicationContext).postsWorkDao(),
+//            )
+//        return try {
+//            repository.processWork(id)
+//            Result.success()
+//        } catch (e: Exception) {
+//            Result.retry()
+//        }
+//    }
+//}
+//
+//class SaveJobWorker(
+//    applicationContext: Context,
+//    params: WorkerParameters
+//) : CoroutineWorker(applicationContext, params) {
+//    companion object {
+//        const val jobKey = "job"
+//    }
+//
+//    override suspend fun doWork(): Result {
+//        val id = inputData.getLong(jobKey, 0L)
+//        if (id == 0L) {
+//            return Result.failure()
+//        }
+//        val repository: Repository<Job> =
+//            JobRepositoryImpl(
+//                AppDbJob.getInstance(context = applicationContext).jobsDao(),
+//                AppDbJob.getInstance(context = applicationContext).jobsWorkDao(),
+//            )
+//        return try {
+//            repository.processWork(id)
+//            Result.success()
+//        } catch (e: Exception) {
+//            Result.retry()
+//        }
+//    }
+//}
+//
+//class SaveUserWorker(
+//    applicationContext: Context,
+//    params: WorkerParameters
+//) : CoroutineWorker(applicationContext, params) {
+//    companion object {
+//        const val userKey = "user"
+//    }
+//
+//    override suspend fun doWork(): Result {
+//        val id = inputData.getLong(userKey, 0L)
+//        if (id == 0L) {
+//            return Result.failure()
+//        }
+//        val repository: Repository<User> =
+//            UserRepositoryImpl(
+//                AppDbUser.getInstance(context = applicationContext).usersDao(),
+//                AppDbUser.getInstance(context = applicationContext).usersWorkDao(),
+//            )
+//        return try {
+//            repository.processWork(id)
+//            Result.success()
+//        } catch (e: Exception) {
+//            Result.retry()
+//        }
+//    }
+//}
