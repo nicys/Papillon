f28b4c4a0bfd21c0c91a82e1ea73d01054867bed
nicys <nicys2476@yandex.ru>
2021-11-20 13:21:04 +0700

    added RefreshPosts.kt

---
+++ app/src/main/java/ru/netology/papillon/work/RefreshPosts.kt
@@ -0,0 +1,34 @@
+package ru.netology.papillon.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import ru.netology.papillon.db.AppDbPost
+import ru.netology.papillon.repository.PostRepositoryImpl
+
+class RefreshPostsWorker(
+    applicationContext: Context,
+    params: WorkerParameters
+) : CoroutineWorker(applicationContext, params) {
+    companion object {
+        const val name = "ru.netology.papillon.work.RefreshPostsWorker"
+    }
+
+    override suspend fun doWork(): Result = withContext(Dispatchers.Default) {
+        val repository: PostRepositoryImpl =
+            PostRepositoryImpl(
+                AppDbPost.getInstance(context = applicationContext).postsDao(),
+//                AppDbPost.getInstance(context = applicationContext).postsWorkDao(),
+            )
+
+        try {
+            repository.getAll()
+            Result.success()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Result.retry()
+        }
+    }
+}
