66437c84d04730ca82a410da54fc8f16b1476fa8
nicys <nicys2476@yandex.ru>
2021-11-23 10:29:36 +0700

    corrected val entered: String = "2021-08-17T16:46:58.887547Z" at User.kt

--- app/src/main/java/ru/netology/papillon/dto/User.kt
+++ app/src/main/java/ru/netology/papillon/dto/User.kt
@@ -7,7 +7,7 @@ import kotlinx.parcelize.Parcelize
 data class User(
     val idUser: Long = 0L,
     val avatar: String? = null,
-    val entered: String = "",
+    val entered: String = "2021-08-17T16:46:58.887547Z",
     val name: String = "",
     val login: String = "",
     val isMe: Boolean = false,
